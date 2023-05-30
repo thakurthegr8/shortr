9495576ca683cf60310700ae8b6fe9234bbe8121
Rahultadmybrand <rahul.thakur@admybrand.com>
2023-05-30 13:55:44 +0530

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import "react-toastify/dist/ReactToastify.css";
 import "@/styles/globals.css";
 import { ToastContainer } from "react-toastify";
 import AuthProvider from "@/src/providers/Auth";
+import {Analytics} from "@vercel/analytics"
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <AuthProvider>
         <Component {...pageProps} />
+        <Analytics/>
         <ToastContainer position="bottom-right" />
       </AuthProvider>
     </>
